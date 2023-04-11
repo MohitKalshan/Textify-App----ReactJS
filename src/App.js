f43eee5c3f90042af5fa2b1bb8242e0726b46e6e
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-04-11 22:57:08 +0530

    alert component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
@@ -7,6 +8,13 @@ import React, { useState } from "react";
 function App() {
 
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message,type)=>{
+    setAlert({
+      message: message,
+      type: type
+    })
+  }
   const toggleMode= ()=>{
     if (mode==="light"){
       setMode("dark");
@@ -21,6 +29,7 @@ function App() {
   return (
     <>
       <Navbar title="Textify" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
         <TextForm heading="What do you textify?" mode={mode}/>
       </div>
