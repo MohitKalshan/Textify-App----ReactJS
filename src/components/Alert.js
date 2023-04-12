ea5e7a1aec7c74c86d7bcff4eef280c4f9bb0e99
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-04-12 18:17:58 +0530

    Capitalize the first character of alert name

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Alert(props) {
+  // Capitalize the first character of alert name
+  const capitalized = (word)=>{
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
   return (
       props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{props.alert.type}</strong>: {props.alert.message}
+        <strong>{capitalized(props.alert.type)}</strong>: {props.alert.message}
         <button
           type="button"
           className="btn-close"
