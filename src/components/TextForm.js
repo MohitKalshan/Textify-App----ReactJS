56a003a67c809c01def99d6896be99aace7b4c78
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-04-15 18:33:05 +0530

    bugs fixed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -115,7 +115,7 @@ export default function TextForm(props) {
         <p>
           {text.split(" ").filter((elem)=>{ return elem.length!==0}).length} Word and {text.length} characters
         </p>
-        <p> {0.08 * text.split(" ").length} Minutes to read</p>
+        <p> {0.08 * text.split(" ").filter((elem)=>{ return elem.length!==0}).length} Minutes to read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter text to preview"}</p>
       </div>
